00eb9efe072a6a3374bba4c43b2a6c1aa9932cb7
epaulett3 <jc.paul.pospos@gmail.com>
2024-08-25 21:46:07 +0800

    Header section update, Create Custom Page Template and Added CSS

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -2,3 +2,12 @@
 
 // add custom php functions below
 include( get_stylesheet_directory() . '/testwork/main.functions.php' );
+
+// creates a custom action hook and hook the storefront_page_content
+add_action('cu_storefront_page', 'storefront_page_content', 10);
+
+
+function storefront_header_container() {
+    echo '<div class="cu-branding">';
+}
+
